6fcc11f0268f3b7fc1a6ad6f7700eefa0d30a802
patriciasouzas <patriciasouzaa89@gmail.com>
2023-01-16 22:16:58 -0300

    aritmeticos

---
+++ fundamentos/aritmeticos/aritmeticos.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 3
+	b := 2
+
+	fmt.Println("Soma ->", a+b)
+	fmt.Println("Subtração ->", a-b)
+	fmt.Println("Divisão ->", a/b)
+	fmt.Println("Multiplicação ->", a*b)
+}
