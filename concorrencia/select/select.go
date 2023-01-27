21a97d7ef55cab2b23ee84d0628179c06a1077af
patriciasouzas <patriciasouzaa89@gmail.com>
2023-01-27 00:02:33 -0300

    concorrencia

---
+++ concorrencia/select/select.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"time"
+)
+
+func titulo(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+	return c
+}
+
+func oMaisRapido(url1, url2, url3 string) string {
+	c1 := titulo(url1)
+	c2 := titulo(url2)
+	c3 := titulo(url3)
+
+	select {
+	case t1 := <-c1:
+		return t1
+	case t2 := <-c2:
+		return t2
+	case t3 := <-c3:
+		return t3
+	case <-time.After(100 * time.Millisecond):
+		return "Todos perderam!"
+	default:
+		return "Sem resposta ainda."
+	}
+}
+
+func main() {
+	campeao := oMaisRapido(
+		"https://www.youtube.com.br",
+		"https://www.mercadolivre.com.br",
+		"https://www.amazon.com.br",
+	)
+
+	fmt.Println(campeao)
+}
